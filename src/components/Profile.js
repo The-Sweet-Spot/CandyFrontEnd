7f955b622a3600fc00db2768a7bec95f9efa2a78
Tori Deblinger <vdeblinger@yahoo.com>
2022-11-30 15:41:23 -0600

    nothing really

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,8 @@ import React from "react";
 const Profile = () => {
     return (
         <div>
-            <h1>Profile</h1>
+            <h1>Welcome To Your Profile</h1>
+            <h3></h3>
         </div>
     )
 }
