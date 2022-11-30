05d4a8c340a40bd6b54a1d9305b37ac93f789a40
John Tacuri <johnsteven1231@gmail.com>
2022-11-30 13:57:32 -0500

    first test bakery and bakery detail code

--- src/components/Bakery.js
+++ src/components/Bakery.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Bakery = () => {
-    return (
-        <div>
-            <h1>Bakery</h1>
-        </div>
+    const [Bakery, setBakery] = useState ()
+
+useEffect(() => {
+    async function getAllBakedGoods(){
+        const bakedFetch = await fetch(``,{
+            header: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        const pleaseFetch = await bakedFetch.json();
+        console.log(pleaseFetch)
+        setBakery(pleaseFetch)
+    }
+    getAllBakedGoods()
+},[])
+return(
+    Bakery ? Bakery.map((indivBakery, idx) => {
+        console.log ('bakery', indivBakery)
+        return (
+            
+            <div className="Bakery" key={idx}>
+                <h1>Bakery</h1>
+            
+            <div>
+                <p>Image: {indivBakery.image}</p>
+            </div>
+                
+            <div>
+                <p>Name: {indivBakery.bakedGoodsName}</p>
+            </div>
+            </div>
+            )
+        }):<p>No data to show</p>
     )
 }
 
--- src/components/BakeryDetail.js
+++ src/components/BakeryDetail.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 
 const BakeryDetail = () => {
-    return (
-        <div>
-            <h1>Bakery Detail</h1>
+     const [BakeryDetail, setBakeryDetail] = useState ()
+
+useEffect (() => {
+    async function getAllBakedGoodsById() {
+        const bakedDetailFetch = await fetch(``, {
+            header:{
+                'Content-Type' : 'application/json'
+            }
+        })
+        const detailBakeryFetch = await bakedDetailFetch.json();
+        console.log(detailBakeryFetch)
+        setBakeryDetail(detailBakeryFetch)
+    }
+    getAllBakedGoodsById()
+}, [])
+return (
+    BakeryDetail ? BakeryDetail.map((indivBakeryDetail, idx) => {
+        console.log('bakeryDetail', indivBakeryDetail)
+        return (
+            <div>
+                <h1>Bakery Detail</h1>
+            
+            <div>
+                <p>Price: {indivBakeryDetail.price}</p>
+            </div>
+
+            <div>
+                <p>Description: {indivBakeryDetail.bakedDescription}</p>
+            </div>
+
+            <div>
+                <p>Stock: {indivBakeryDetail.stock}</p>
+            </div>
         </div>
+            )
+        }):<p>No data to show</p>
     )
 }
 
