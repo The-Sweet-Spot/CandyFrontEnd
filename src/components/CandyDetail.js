9fdde4fade0fe3193665bacb0a81703323afb1ee
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-30 16:00:12 -0600

    candy detail is not finished, just saving

--- src/components/CandyDetail.js
+++ src/components/CandyDetail.js
@@ -1,23 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; 
 
-const CandyDetail = (props) => {
-    const candyData = props;
+const CandyDetail = () => {
+    const [candyDetails, setCandyDetails] = useState ([])
+    const {id} = useParams ()
+
+    useEffect(() => {
+        async function fetchCandyId(){
+            try{
+                const fetchedCandy = await fetch(`https://backend-sweet-spot.onrender.com/api/candy/${id}`,
+                {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                const translatedCandyId = await fetchedCandy.json();
+                    // console.log("Here's the translated json", translatedCandyId)
+                setCandy(translatedCandyId);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+            fetchCandyId();
+    },[])
+
     
     return (
-        <div>
 
-            {/* Details for Each Candy Listing */}
-            <h2>{candyData.candyId.candyName}</h2>
-            <p>{candyData.candyId.candyDescription}</p>
-            <p><strong>Price: </strong>{candyData.candyId.price}</p>
-            <p>{candyData.candyId.image}</p>
+        candyDetails ? candyDetails.map((indivCandyDetail, idx) => {
+            return(
 
-            {/* <div id="button-container">
-                <button id="review-btn"><Link to={`/candy/${candyId}`}>Leave Review</Link></button>
-            </div> */}
-        </div>
+                <div key={idx}>
+
+                    <img src={indivCandyDetail.image}></img>
+
+                    <h2>Name: {indivCandyDetail.candyName}</h2>
+                    <h4>Price: {indivCandyDetail.price}</h4>
+                </div>
+            )
+        }) : "We're all sold out of Candy, Whoopsy! "
     )
 };
+
 
 export default CandyDetail
