b86f232ba89046c3e83f7bae6755b123e99a8fb0
Marc Hernandez <drstein21@live.com>
2022-11-30 12:57:23 -0600

    rough draft

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function handleUsernameChange(event) {
+        console.log(event.target.value);
+        setUsername(event.target.value);
+    }
+    function handlePasswordChange(event) {
+        console.log(event.target.value);
+        setPassword(event.target.value);
+    }
+
+    async function setRegisterInfo(event) {
+        event.preventDefault()
+        try {
+            const response = await fetch(
+                '',
+                {
+                    method:"POST",
+                    headers: {
+                        "Content-Type": "application/json",
+
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        password: password
+                    })
+                }
+            )
+            const data = await response.json();
+            console.log("Request's returned promise: ", data.token);
+            localStorage.setItem("token", data.token);
+            navigate("/Homepage")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>
+        <div> 
+            <div>
             <h1>Login</h1>
+            <br/>
+            <form onSubmit={setRegisterInfo}>
+                <label>Username:</label><br/>
+                <label>Password:</label>
+                <Button type="submit">Login</Button>
+            </form>
+            <p>Don't have a login? <Link to ="Register"> Register Here!</Link></p>
+            </div>
         </div>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const navigate = userNavigate();
+
+    async function registerHandler(event){
+        event.preventDefault();
+        try {
+            const response = await fetch('', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+            const data = await response.json()
+            localStorage.setItem("token", data.token)
+            console.log(data.token)
+            console.log(data)
+
+            navigate("/Homepage")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const changeUsername = (event) => {
+        setUsername(event.target.value)
+    }
+    const changePassword = (event) => {
+        setPassword(event.target.value)
+    }
     return (
         <div>
+            <div>
             <h1>Register</h1>
+            </div>
         </div>
     )
 }
