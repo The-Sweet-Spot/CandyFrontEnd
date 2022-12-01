779fdd1bfcaefed4e80ed15a4744c318d7368f9a
John Tacuri <johnsteven1231@gmail.com>
2022-12-01 13:43:09 -0500

    bakery detail working

--- src/components/Bakery.js
+++ src/components/Bakery.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 const Bakery = () => {
-    const [bakery, setBakery] = useState ([])
+    const [bakery, setBakery] = useOutletContext()
 
-useEffect(() => {
-    async function getAllBakedGoods(){
-        const bakedFetch = await fetch(`https://backend-sweet-spot.onrender.com/api/bakery`,{
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        })
-        const pleaseFetch = await bakedFetch.json();
-        console.log(pleaseFetch)
-        setBakery(pleaseFetch)
-    }
-    getAllBakedGoods()
-},[])
+// useEffect(() => {
+//     async function getAllBakedGoods(){
+//         const bakedFetch = await fetch(`https://backend-sweet-spot.onrender.com/api/bakery`,{
+//             headers: {
+//                 'Content-Type' : 'application/json'
+//             }
+//         })
+//         const pleaseFetch = await bakedFetch.json();
+//         console.log(pleaseFetch)
+//         setBakery(pleaseFetch)
+//     }
+//     getAllBakedGoods()
+// },[])
 return(
     
-    bakery ? bakery.map((indivBakery) => {
+    bakery ? bakery.map((indivBakery,idx) => {
         return (
             
-            <div>
+            <div key={idx}>
                 <h1>Bakery</h1>
             
            <div>
@@ -34,7 +34,7 @@ return(
             </h3>
             <div>
                 <button>
-                    <Link className="BakeryButton link" to={`/bakery/${indivBakery.id}`}>Details</Link>
+                    <Link className="BakeryButton link" to={`/bakery/${indivBakery.bakedId}`}>Details</Link>
                 </button>
             </div>
             </div>
--- src/components/BakeryDetail.js
+++ src/components/BakeryDetail.js
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import React, { useState, useEffect} from "react"
+import { useNavigate, useOutletContext, useParams } from "react-router-dom"
 
 const BakeryDetail = () => {
-     const [BakeryDetail, setBakeryDetail] = useState ();
-    const {bakedId} = useParams ();
+    const [bakery, setBakery] = useOutletContext()
+    const [moreBakeryDetail, setMoreBakeryDetail] = useState({})
+    const navigate = useNavigate()
+    const {bakedId} = useParams()
+    console.log(bakedId)
+    console.log(bakery)
+
 useEffect (() => {
-    async function getAllBakedGoodsById() {
-        const bakedDetailFetch = await fetch(`https://backend-sweet-spot.onrender.com/api/bakery/${bakedId}`, {
-            header:{
-                'Content-Type' : 'application/json'
-            }
-        })
-        const detailBakeryFetch = await bakedDetailFetch.json();
-        console.log(detailBakeryFetch)
-        setBakeryDetail(detailBakeryFetch)
-    }
-    getAllBakedGoodsById()
+        async function bakedDetailFetch(){
+    const bakedDetailFetch = await fetch(`https://backend-sweet-spot.onrender.com/api/bakery/${bakedId}`, {
+        header:{
+            'Content-Type' : 'application/json'
+        }
+    })
+    const detailBakeryFetch = await bakedDetailFetch.json();
+    console.log(detailBakeryFetch)
+    setMoreBakeryDetail(detailBakeryFetch)
+}
+    bakedDetailFetch()
 }, [])
-return (
-    BakeryDetail ? BakeryDetail.map((indivBakeryDetail, idx) => {
-        console.log('bakeryDetail', indivBakeryDetail)
-        return (
-            <div>
-                <h1>Bakery Detail</h1>
-            
-            <div>
-                <p>Price: {indivBakeryDetail.price}</p>
-            </div>
 
-            <div>
-                <p>Description: {indivBakeryDetail.bakedDescription}</p>
-            </div>
-
-            <div>
-                <p>Stock: {indivBakeryDetail.stock}</p>
-            </div>
+    return (
+        <div>
+            {
+                moreBakeryDetail.bakedGoodsName ?
+                <p id ="description">{moreBakeryDetail.bakedGoodsName}</p>:
+                <p>Description can not be viewed</p>
+            }
+            {
+                moreBakeryDetail.bakedDescription ?
+                <p id="description">{moreBakeryDetail.bakedDescription}</p>:
+                <p>Description can not be viewed</p>
+            }
+            {
+                moreBakeryDetail.price ?
+                <p id="description">{moreBakeryDetail.price}</p>:
+                <p>Description can not be viewed</p>
+            }
         </div>
-            )
-        }):<p>No data to show</p>
     )
 }
 
