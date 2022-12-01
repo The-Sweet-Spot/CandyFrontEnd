5425e86a3c6c32a4385749c44aa629b3e0b1ee15
Marc Hernandez <drstein21@live.com>
2022-12-01 12:53:53 -0600

    updated profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const Profile = () => {
-    return (
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!localStorage.getItem("token")) {
+            navigate('/login')
+        }
+            async function fetchProfileData() {
+                try {
+                    const response = await fetch('https://backend-sweet-spot.onrender.com/api/users/me',
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": `Bearer ${localStorage.getItem("token")}`
+                        }
+                    }
+                );
+                const data = await response.json();
+                console.log("This is the profile data: ", data)
+                } catch (error) {
+                    console.log(error);
+                }
+                
+            }
+            fetchProfileData();
+        }
+    )
+        
+    
+    function logOut(event) {
+        localStorage.removeItem("token");
+        navigate("/")
+    }
+    return(
         <div>
-            <h1>Welcome To Your Profile</h1>
-            <h3></h3>
+            <h3>Your Profile</h3>
+            <p>Welcome Back!</p>
+            <div>
+                <button onClick={logOut}>Log Out</button>
+            </div>
         </div>
     )
 }
-
 export default Profile
